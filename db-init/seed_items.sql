426310496f4463ad372aa2af1753cee99f04cf35
Andeilso Alves <andeilsoalves@gmail.com>
2025-08-28 15:33:25 -0300

    New seed file created to populate the items table in the database mount in docker.

---
+++ db-init/seed_items.sql
@@ -0,0 +1,15 @@
+-- TABLE ITEMS
+INSERT INTO items (item_id, item_name, item_description) VALUES
+(1, 'master-ball', 'Used in battle: Catches a wild Pokémon without fail. If used in a trainer battle, nothing happens and the ball is lost.'),
+(2, 'ultra-ball', 'Used in battle: Attempts to catch a wild Pokémon, using a catch rate of 2. If used in a trainer battle, nothing happens and the ball is lost.'),
+(3, 'great-ball', 'Used in battle: Attempts to catch a wild Pokémon, using a catch rate of 1.5. If used in a trainer battle, nothing happens and the ball is lost.'),
+(4, 'poke-ball', 'Used in battle: Attempts to catch a wild Pokémon, using a catch rate of 1. If used in a trainer battle, nothing happens and the ball is lost.'),
+(17, 'potion', 'Used on a friendly Pokémon: Restores 20 HP.'),
+(23, 'full-restore', 'Used on a party Pokémon: Restores HP to full and cures any status ailment and confusion.'),
+(24, 'max-potion', 'Used on a party Pokémon: Restores HP to full.'),
+(25, 'hyper-potion', 'Used on a party Pokémon: Restores 200 HP.'),
+(26, 'super-potion', 'Used on a party Pokémon: Restores 50 HP.'),
+(28, 'revive', 'Used on a party Pokémon: Revives the Pokémon and restores half its HP.'),
+(29, 'max-revive', 'Used on a party Pokémon: Revives the Pokémon and restores its HP to full.'),
+(50, 'rare-candy', 'Used on a party Pokémon: Increases level by 1. Increases happiness by 5/3/2.');
+
