976d3d6722700b9085d23dd399afc2add47c54b4
Andeilso Alves <andeilsoalves@gmail.com>
2025-08-28 15:46:43 -0300

    API root changed, methods added in cors.

--- back-end/index.js
+++ back-end/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const usersRoutes = require('./routes/usersRoutes.js');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/routes.js';
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
-    methods: ['GET'],
+    origin: 'http://127.0.0.1:5000',
+    methods: ['GET', 'POST', 'UPDATE', 'DELETE'],
     //credentials: true
 }))
 
 app.use(express.json());
 
-app.use('/api', usersRoutes);
+app.use('/', routes);
 
 app.listen(4000, () => {
     console.log('Servidor rodando na porta 4000');
